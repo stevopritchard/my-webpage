b5327156e796186ccebd46a6b5d340f3934663f4
stevopritchard <stephen.a.pritchard@gmail.com>
2022-07-25 21:26:59 +0100

    updated 'About me' statement

--- src/Containers/AboutMe.js
+++ src/Containers/AboutMe.js
@@ -4,12 +4,18 @@ import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import { Grid, Divider, Typography, SvgIcon } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faNodeJs, faHtml5, faCss3, faJs} from '@fortawesome/free-brands-svg-icons';
-import {ReactComponent as Expressjs } from '../expressjs-icon.svg';
-import {ReactComponent as Postgresql} from '../postgresql.svg';
-import {ReactComponent as Mongodb} from '../mongodb.svg';
+import {
+  faReact,
+  faNodeJs,
+  faHtml5,
+  faCss3,
+  faJs,
+} from '@fortawesome/free-brands-svg-icons';
+import { ReactComponent as Expressjs } from '../expressjs-icon.svg';
+import { ReactComponent as Postgresql } from '../postgresql.svg';
+import { ReactComponent as Mongodb } from '../mongodb.svg';
 import Popover from '@material-ui/core/Popover';
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles';
 import { Tabs, Tab } from '@material-ui/core';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -26,12 +32,12 @@ const useStyles = makeStyles({
   },
   button: {
     border: '2px solid',
-    borderRadius: '11px'
-  }
-})
+    borderRadius: '11px',
+  },
+});
 
 function TabPanel(props) {
-  const { children, value, index, ...other} = props;
+  const { children, value, index, ...other } = props;
 
   return (
     <div
@@ -41,20 +47,16 @@ function TabPanel(props) {
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && (
-        <Box>
-          {children}
-        </Box>
-      )}
+      {value === index && <Box>{children}</Box>}
     </div>
-  )
+  );
 }
 
 function a11yProps(index) {
   return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`
-  }
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
 }
 
 function createFEData(school, html5, css3, js, react) {
@@ -66,16 +68,40 @@ function createBEData(school, nodejs, express, postgresql, mongodb) {
 }
 
 const frontEndCourses = [
-  createFEData('Complete Web Developer: Zero to Mastery (Udemy)' ,'',<CheckIcon style={{height:13, width: 13}}/>,<CheckIcon style={{height:13, width: 13}}/>,<CheckIcon style={{height:13, width: 13}}/>),
-  createFEData('Complete Node.js Developer Course (Udemy)','','','',''),
-  createFEData('freecodecamp',<CheckIcon style={{height:13, width: 13}}/>,<CheckIcon style={{height:13, width: 13}}/>,'','')
-]
+  createFEData(
+    'Complete Web Developer: Zero to Mastery (Udemy)',
+    '',
+    <CheckIcon style={{ height: 13, width: 13 }} />,
+    <CheckIcon style={{ height: 13, width: 13 }} />,
+    <CheckIcon style={{ height: 13, width: 13 }} />
+  ),
+  createFEData('Complete Node.js Developer Course (Udemy)', '', '', '', ''),
+  createFEData(
+    'freecodecamp',
+    <CheckIcon style={{ height: 13, width: 13 }} />,
+    <CheckIcon style={{ height: 13, width: 13 }} />,
+    '',
+    ''
+  ),
+];
 
 const backEndCourses = [
-  createBEData('Complete Web Developer: Zero to Mastery (Udemy)' ,<CheckIcon style={{height:13, width: 13}}/>,<CheckIcon style={{height:13, width: 13}}/>,<CheckIcon style={{height:13, width: 13}}/>,''),
-  createBEData('Complete Node.js Developer Course (Udemy)',<CheckIcon style={{height:13, width: 13}}/>,<CheckIcon style={{height:13, width: 13}}/>,<CheckIcon style={{height:13, width: 13}}/>,<CheckIcon style={{height:13, width: 13}}/>),
-  createBEData('freecodecamp','','','','')
-]
+  createBEData(
+    'Complete Web Developer: Zero to Mastery (Udemy)',
+    <CheckIcon style={{ height: 13, width: 13 }} />,
+    <CheckIcon style={{ height: 13, width: 13 }} />,
+    <CheckIcon style={{ height: 13, width: 13 }} />,
+    ''
+  ),
+  createBEData(
+    'Complete Node.js Developer Course (Udemy)',
+    <CheckIcon style={{ height: 13, width: 13 }} />,
+    <CheckIcon style={{ height: 13, width: 13 }} />,
+    <CheckIcon style={{ height: 13, width: 13 }} />,
+    <CheckIcon style={{ height: 13, width: 13 }} />
+  ),
+  createBEData('freecodecamp', '', '', '', ''),
+];
 
 function FrontEndTable() {
   return (
@@ -83,18 +109,24 @@ function FrontEndTable() {
       <TableHead>
         <TableRow>
           <TableCell></TableCell>
-          <TableCell><FontAwesomeIcon icon={faHtml5} size='sm' /></TableCell>
-          <TableCell><FontAwesomeIcon icon={faCss3} size='sm' /></TableCell>
-          <TableCell><FontAwesomeIcon icon={faJs} size='sm' /></TableCell>
-          <TableCell><FontAwesomeIcon icon={faReact} size='sm' /></TableCell>
+          <TableCell>
+            <FontAwesomeIcon icon={faHtml5} size="sm" />
+          </TableCell>
+          <TableCell>
+            <FontAwesomeIcon icon={faCss3} size="sm" />
+          </TableCell>
+          <TableCell>
+            <FontAwesomeIcon icon={faJs} size="sm" />
+          </TableCell>
+          <TableCell>
+            <FontAwesomeIcon icon={faReact} size="sm" />
+          </TableCell>
         </TableRow>
       </TableHead>
       <TableBody>
         {frontEndCourses.map((row) => (
-          <TableRow
-            key={row.school}
-          >
-            <TableCell component='th' scope='row'>
+          <TableRow key={row.school}>
+            <TableCell component="th" scope="row">
               {row.school}
             </TableCell>
             <TableCell align="right">{row.html5}</TableCell>
@@ -105,7 +137,7 @@ function FrontEndTable() {
         ))}
       </TableBody>
     </Table>
-  )
+  );
 }
 
 function BackEndTable() {
@@ -114,18 +146,30 @@ function BackEndTable() {
       <TableHead>
         <TableRow>
           <TableCell></TableCell>
-          <TableCell><FontAwesomeIcon icon={faNodeJs} size='sm' /></TableCell>
-          <TableCell><SvgIcon viewBox='0 0 70 40' style={{height:20, width: 20}}><Expressjs /></SvgIcon></TableCell>
-          <TableCell><SvgIcon viewBox='0 -8 58 43' style={{height:18, width: 18}}><Postgresql /></SvgIcon></TableCell>
-          <TableCell><SvgIcon viewBox='0 -5 50 40' style={{height:19, width: 19}}><Mongodb /></SvgIcon></TableCell>
+          <TableCell>
+            <FontAwesomeIcon icon={faNodeJs} size="sm" />
+          </TableCell>
+          <TableCell>
+            <SvgIcon viewBox="0 0 70 40" style={{ height: 20, width: 20 }}>
+              <Expressjs />
+            </SvgIcon>
+          </TableCell>
+          <TableCell>
+            <SvgIcon viewBox="0 -8 58 43" style={{ height: 18, width: 18 }}>
+              <Postgresql />
+            </SvgIcon>
+          </TableCell>
+          <TableCell>
+            <SvgIcon viewBox="0 -5 50 40" style={{ height: 19, width: 19 }}>
+              <Mongodb />
+            </SvgIcon>
+          </TableCell>
         </TableRow>
       </TableHead>
       <TableBody>
         {backEndCourses.map((row) => (
-          <TableRow
-            key={row.school}
-          >
-            <TableCell component='th' scope='row'>
+          <TableRow key={row.school}>
+            <TableCell component="th" scope="row">
               {row.school}
             </TableCell>
             <TableCell align="right">{row.nodejs}</TableCell>
@@ -136,180 +180,238 @@ function BackEndTable() {
         ))}
       </TableBody>
     </Table>
-  )
+  );
 }
 
 const Popover1 = () => {
   return (
-    <Box display='flex' flexDirection='row' margin={1.8}>
-      <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' padding={1.5}>
+    <Box display="flex" flexDirection="row" margin={1.8}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        padding={1.5}
+      >
         <Box>
-          <Typography style={{ fontWeight: 500 }} >Front-End</Typography>
+          <Typography style={{ fontWeight: 500 }}>Front-End</Typography>
         </Box>
-        <Box display='flex' flexDirection='row' style={{paddingTop: '9px'}}>
+        <Box display="flex" flexDirection="row" style={{ paddingTop: '9px' }}>
           <Box paddingLeft={2} paddingRight={1.3}>
-            <FontAwesomeIcon icon={faJs} size='2x' />
+            <FontAwesomeIcon icon={faJs} size="2x" />
           </Box>
           <Box paddingRight={1.3}>
-            <FontAwesomeIcon icon={faHtml5} size='2x' />
+            <FontAwesomeIcon icon={faHtml5} size="2x" />
           </Box>
           <Box paddingRight={2}>
-            <FontAwesomeIcon icon={faCss3} size='2x' />
+            <FontAwesomeIcon icon={faCss3} size="2x" />
           </Box>
         </Box>
       </Box>
-      <Divider orientation="vertical" flexItem/>
-      <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center' padding={1.5}>
+      <Divider orientation="vertical" flexItem />
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        padding={1.5}
+      >
         <Box>
-          <Typography style={{ fontWeight: 500 }} >Back-End</Typography>
+          <Typography style={{ fontWeight: 500 }}>Back-End</Typography>
         </Box>
-        <Box display='flex' flexDirection='row' alignItems='center'>
+        <Box display="flex" flexDirection="row" alignItems="center">
           <Box paddingLeft={2} paddingRight={0.6} marginTop={-0.2}>
-            <SvgIcon viewBox='0 10 70 40' style={{height:35, width: 35}}>
-              <Expressjs/>
+            <SvgIcon viewBox="0 10 70 40" style={{ height: 35, width: 35 }}>
+              <Expressjs />
             </SvgIcon>
           </Box>
-          <Box  marginBottom={-0.9}>
-            <SvgIcon viewBox='5 0 50 40' style={{height:35, width: 35}}>
+          <Box marginBottom={-0.9}>
+            <SvgIcon viewBox="5 0 50 40" style={{ height: 35, width: 35 }}>
               <Postgresql />
             </SvgIcon>
           </Box>
           <Box>
-            <SvgIcon viewBox='8 -2 35 45' style={{height:35, width: 36}}>
+            <SvgIcon viewBox="8 -2 35 45" style={{ height: 35, width: 36 }}>
               <Mongodb />
             </SvgIcon>
           </Box>
         </Box>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
 const Popover2 = () => {
-  const [activeTab, setActiveTab] = React.useState(0)
+  const [activeTab, setActiveTab] = React.useState(0);
 
   const handleTabChange = (event, newValue) => {
     setActiveTab(newValue);
-  }
-  
+  };
+
   return (
     <Box>
       <Box>
-        <Tabs value={activeTab} onChange={handleTabChange} aria-label="simple table" centered={true}>
-          <Tab style={{ fontWeight: 500 }} label="Front-End" {...a11yProps(0)} />
+        <Tabs
+          value={activeTab}
+          onChange={handleTabChange}
+          aria-label="simple table"
+          centered={true}
+        >
+          <Tab
+            style={{ fontWeight: 500 }}
+            label="Front-End"
+            {...a11yProps(0)}
+          />
           <Tab style={{ fontWeight: 500 }} label="Back-End" {...a11yProps(1)} />
         </Tabs>
       </Box>
-      <TabPanel 
-        value={activeTab}
-        index={0}
-      >
+      <TabPanel value={activeTab} index={0}>
         <FrontEndTable />
       </TabPanel>
-      <TabPanel 
-        value={activeTab}
-        index={1}
-      >
+      <TabPanel value={activeTab} index={1}>
         <BackEndTable />
       </TabPanel>
     </Box>
-  )
-}
+  );
+};
 
 export default function AboutMe() {
-    const [anchorEl1, setAnchorEl1] = React.useState({
-      anchorEl: null,
-      child: <Popover1 />
-    })
+  const [anchorEl1, setAnchorEl1] = React.useState({
+    anchorEl: null,
+    child: <Popover1 />,
+  });
 
-    const [anchorEl2, setAnchorEl2] = React.useState({
-      anchorEl: null,
-      child: <Popover2 />
-    })
+  const [anchorEl2, setAnchorEl2] = React.useState({
+    anchorEl: null,
+    child: <Popover2 />,
+  });
 
+  const handleClick1 = (event) => {
+    setAnchorEl1({ ...anchorEl1, anchorEl: event.currentTarget });
+  };
 
-    const handleClick1 = (event) => {
-      setAnchorEl1({...anchorEl1, anchorEl:event.currentTarget});
-    }
+  const handleClick2 = (event) => {
+    setAnchorEl2({ ...anchorEl2, anchorEl: event.currentTarget });
+  };
 
-    const handleClick2 = (event) => {
-      setAnchorEl2({...anchorEl2, anchorEl:event.currentTarget});
-    }
+  const open1 = Boolean(anchorEl1.anchorEl);
+  const id1 = open1 ? 'skills-popover' : undefined;
 
-    const open1 = Boolean(anchorEl1.anchorEl);
-    const id1 = open1 ? 'skills-popover' : undefined;
+  const open2 = Boolean(anchorEl2.anchorEl);
+  const id2 = open2 ? 'courses-popover' : undefined;
 
-    const open2 = Boolean(anchorEl2.anchorEl);
-    const id2 = open2 ? 'courses-popover' : undefined;
-
-    const classes = useStyles();
-    return (
-      <Container className={classes.mainArea} fixed>
-        <Box  display='flex' flexDirection='row' justifyContent='center' paddingTop='2em' paddingBottom='2em'>
-            <Typography variant="h2" align='center' style={{fontWeight: 600}}>Stephen Pritchard</Typography>
-        </Box>
-        <Typography variant="body1" align='center'>
-          An aspiring full stack developer looking for a new challenge! {<br/>}
+  const classes = useStyles();
+  return (
+    <Container className={classes.mainArea} fixed>
+      <Box
+        display="flex"
+        flexDirection="row"
+        justifyContent="center"
+        paddingTop="2em"
+        paddingBottom="2em"
+      >
+        <Typography variant="h2" align="center" style={{ fontWeight: 600 }}>
+          Stephen Pritchard
         </Typography>
-        <Typography variant="body1" align='justify'>
-          I have been learning to code alongside my current job in retail finance and have taken on several freelance 
-          projects over the past couple of years with a view to making the leap into a full-time role.
-        </Typography>
-        <Box display='flex' flexDirection='row' justifyContent='space-around' paddingTop='30px'>
-          <Box display='flex' flexDirection='column' alignItems="center">
-            <FontAwesomeIcon icon={faReact} size='5x' />
-            <Typography variant="h5" style={{fontWeight: 'bold'}}>React</Typography>
-          </Box>
-          <Box display='flex' flexDirection='column' alignItems="center">
-            <FontAwesomeIcon icon={faNodeJs} size='5x' />
-            <Typography variant="h5" style={{fontWeight: 'bold'}}>Node.js</Typography>
-          </Box>
+      </Box>
+      <Typography variant="body1" align="center">
+        Creative and proactive Full Stack Developer currently supporting
+        multiple projects across Lloyd's Banking Group's Chief Customer Office.{' '}
+        {<br />}
+      </Typography>
+      <Typography variant="body1" align="justify">
+        My passion is to make interactions easier for end-users, colleagues, and
+        the business by developing solutions that are accessible, fast and that
+        empower users by being easy to use. I want to bring my skillset to your
+        development team and help to build the great things that you are working
+        on in JavaScript.
+      </Typography>
+      <Box
+        display="flex"
+        flexDirection="row"
+        justifyContent="space-around"
+        paddingTop="30px"
+      >
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <FontAwesomeIcon icon={faReact} size="5x" />
+          <Typography variant="h5" style={{ fontWeight: 'bold' }}>
+            React
+          </Typography>
         </Box>
-        <Box paddingTop='3em' display='flex' flexWrap='wrap' flexDirection='row' justifyContent='space-around'>
-        <Grid container spacing={2} >
-          <Grid item xs={12} sm={6} md={6} style={{display: 'flex', justifyContent: 'center'}}>
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <FontAwesomeIcon icon={faNodeJs} size="5x" />
+          <Typography variant="h5" style={{ fontWeight: 'bold' }}>
+            Node.js
+          </Typography>
+        </Box>
+      </Box>
+      <Box
+        paddingTop="3em"
+        display="flex"
+        flexWrap="wrap"
+        flexDirection="row"
+        justifyContent="space-around"
+      >
+        <Grid container spacing={2}>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={6}
+            style={{ display: 'flex', justifyContent: 'center' }}
+          >
             <Button
               aria-describedby={id1}
               className={classes.button}
               onClick={handleClick1}
               variant="outlined"
             >
-              <Typography style={{fontSize:'0.7em', fontWeight: 500}}>See the rest of my skills...</Typography>
+              <Typography style={{ fontSize: '0.7em', fontWeight: 500 }}>
+                See the rest of my skills...
+              </Typography>
             </Button>
           </Grid>
-          <Grid item xs={12} sm={6} md={6} style={{display: 'flex', justifyContent: 'center'}}>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={6}
+            style={{ display: 'flex', justifyContent: 'center' }}
+          >
             <Button
               aria-describedby={id2}
               className={classes.button}
               onClick={handleClick2}
               variant="outlined"
             >
-              <Typography style={{fontSize:'0.7em', fontWeight: 500}}>...and where I got them.</Typography>
+              <Typography style={{ fontSize: '0.7em', fontWeight: 500 }}>
+                ...and where I got them.
+              </Typography>
             </Button>
           </Grid>
-            <Popover
-              id={id1}
-              open={open1}
-              anchorEl={anchorEl1.anchorEl}
-              onClose={() => setAnchorEl1({ ...anchorEl1, anchorEl: null})}
-              anchorOrigin={{ vertical: 'bottom', horizontal: 'center'}}
-              transformOrigin={{vertical: 'top', horizontal: 'center'}}
-            >
-              {anchorEl1.child}
-            </Popover>
+          <Popover
+            id={id1}
+            open={open1}
+            anchorEl={anchorEl1.anchorEl}
+            onClose={() => setAnchorEl1({ ...anchorEl1, anchorEl: null })}
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+            transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+          >
+            {anchorEl1.child}
+          </Popover>
 
-            <Popover
-              id={id2}
-              open={open2}
-              anchorEl={anchorEl2.anchorEl}
-              onClose={() => setAnchorEl2({ ...anchorEl2, anchorEl: null})}
-              anchorOrigin={{ vertical: 'bottom', horizontal: 'center'}}
-              transformOrigin={{vertical: 'top', horizontal: 'center'}}
-            >
-              {anchorEl2.child}
-            </Popover>
+          <Popover
+            id={id2}
+            open={open2}
+            anchorEl={anchorEl2.anchorEl}
+            onClose={() => setAnchorEl2({ ...anchorEl2, anchorEl: null })}
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+            transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+          >
+            {anchorEl2.child}
+          </Popover>
         </Grid>
-        </Box>
-      </Container>
-    )
-};
+      </Box>
+    </Container>
+  );
+}
